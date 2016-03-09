41aa97497cdfbb0187e25513a88adb36e72382f8
門田 康弘 <monta@eventregist.com>
2016-03-09 16:11:05 +0900

    no message

--- message/_include/header2.php
+++
@@ -1,4 +0,0 @@
-<div id="header2" class="fromyuriko">
-  <p class="alp0" id="mv_p"><img src="./images/mv_01.png" id="mv_ani"></p>
-  <h1 class="alp0" id="mv_h1"><img src="./images/title_01.png" alt="From Yuriko"></h1>
-</div><!-- /header2 -->
--- staffsreport/_include/header2.php
+++
@@ -1,4 +0,0 @@
-<div id="header2" class="staffsreport">
-  <p class="alp0" id="mv_p"><img src="./images/mv_01.png" id="mv_ani"></p>
-  <h1 class="alp0" id="mv_h1"><img src="./images/title_01.png" alt="INFORMATION"></h1>
-</div><!-- /header2 -->
